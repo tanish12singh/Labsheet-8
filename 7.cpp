ad88a8a34afe2e2c2cf577cfb5de06909f7761c7
tanish12singh <tanishsingh11630@gmail.com>
2025-11-10 14:26:59 +0530

    Function to find the sum of two matrices

---
+++ 7.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+void sumMatrix(int A[3][3], int B[3][3], int C[3][3]) {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            C[i][j] = A[i][j] + B[i][j];
+        }
+    }
+}
