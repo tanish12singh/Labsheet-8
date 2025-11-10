dc326ce8fb1b38c44e75e38e18604257d9fba3d2
tanish12singh <tanishsingh11630@gmail.com>
2025-11-10 14:24:56 +0530

    Function to search an element using Binary Search

---
+++ 6.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int binarySearch(int arr[], int n, int key) {
+    int low = 0, high = n - 1;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (arr[mid] == key)
+            return mid;
+        else if (arr[mid] < key)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+    return -1;
+}
